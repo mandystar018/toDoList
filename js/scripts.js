0e53b488aa130bb916cbbb1c510a14a9d3a95d4d
Amanda Otero <mandystar018@gmail.com>
2020-11-02 15:27:28 -0800

    Add ToDoList and ToDo constructor, Add AddTask, assignId, deleteTask prototype. Test out addTask function and assingId function and both pass
    Co-authored-by: Sarah Gilbert <sarahgilbert064@gmail.com>,Co-authored-by: Tyler Sinks <tyler.sinks@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,37 @@
-$(document).ready(function(){
+// function Contact(firstName, lastName, phoneNumber) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.phoneNumber = phoneNumber;
+// }
 
-});
+function ToDoList() {
+  this.tasks = [];
+  this.currentId = 0;
+  //this.item = item;
+}
+
+function ToDo(task) {
+  this.task = task;
+}
+
+ToDoList.prototype.addTask = function(task) {
+  task.id = this.assignId();
+  this.tasks.push(task);
+}
+
+ToDoList.prototype.assignId = function() {
+  this.currentId +=1;
+  return this.currentId;
+}
+
+ToDoList.prototype.deleteTask = function(id) {
+  for (let i = 0; i<this.tasks.length; i++) {
+    if(this.tasks[i]) {
+      if(this.tasks[i].id == id) {
+        delete this.tasks[i];
+        return true;
+      }
+    }
+  };
+  return false;
+}
